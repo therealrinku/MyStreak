0cf863e2c190b49e14f77113ebd0db4e788c3d8a
therealrinku <rinkunited2032@gmail.com>
2026-02-09 10:04:49 +0545

    Add empty state

--- src/renderer/components/backlog.tsx
+++ src/renderer/components/backlog.tsx
@@ -1,19 +1,29 @@
 import TodoItem from './todo-item';
 import AddTodoForm from './add-todo-form';
 import useTodos from '../hooks/use-todos';
+import { GoCheck } from 'react-icons/go';
 
 export default function Backlog() {
   const { todos } = useTodos();
-  const backlog = todos.filter((todo) => todo.backlog === 1);
+  const backlog = todos
+    .filter((todo) => todo.backlog === 1)
+    .sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
 
   return (
     <>
       <AddTodoForm isBacklog={true} />
-      {backlog
-        .sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
-        .map((todo) => {
+      {backlog.length > 0 ? (
+        backlog.map((todo) => {
           return <TodoItem key={todo.id} todo={todo} />;
-        })}
+        })
+      ) : (
+        <div className="flex items-center gap-2 p-3 text-gray-500">
+          <GoCheck size={15} />
+          <b>
+            There are not any backlog items. Looks like you've got it all done!
+          </b>
+        </div>
+      )}
     </>
   );
 }
--- src/renderer/components/todos.tsx
+++ src/renderer/components/todos.tsx
@@ -1,21 +1,36 @@
 import TodoItem from './todo-item';
 import AddTodoForm from './add-todo-form';
 import useTodos from '../hooks/use-todos';
+import { GoBlocked, GoCheck } from 'react-icons/go';
 
 export default function Todos() {
   const { todos } = useTodos();
-  const activeTodos = todos.filter(
-    (todo) => todo.backlog === 0 && todo.completed === 0,
-  );
+  const activeTodos = todos
+    .filter((todo) => todo.backlog === 0 && todo.completed === 0)
+    .sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+
+  const userHasCompletedSomeTodo = todos.find((todo) => todo.completed === 1);
+  const emptyStateMessage = userHasCompletedSomeTodo
+    ? "Yay! you're all done!"
+    : 'There are not any todos yet.';
 
   return (
     <>
       <AddTodoForm />
-      {activeTodos
-        .sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
-        .map((todo) => {
+      {activeTodos.length > 0 ? (
+        activeTodos.map((todo) => {
           return <TodoItem key={todo.id} todo={todo} />;
-        })}
+        })
+      ) : (
+        <div className="flex items-center gap-2 p-3 text-gray-500">
+          {userHasCompletedSomeTodo ? (
+            <GoCheck size={15} />
+          ) : (
+            <GoBlocked size={15} />
+          )}
+          <b>{emptyStateMessage}</b>
+        </div>
+      )}
     </>
   );
 }
