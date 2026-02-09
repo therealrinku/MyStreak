eee5495ade221e6fae65a6e46245bbac308dffb4
therealrinku <rinkunited2032@gmail.com>
2026-02-09 11:14:11 +0545

    Add check button instead of checkbox for marking done

--- src/renderer/components/todo-item.tsx
+++ src/renderer/components/todo-item.tsx
@@ -1,6 +1,7 @@
 import useTodos from '../hooks/use-todos';
 import { useState } from 'react';
 import {
+  GoCheckCircle,
   GoArrowBoth,
   GoArrowSwitch,
   GoChevronUp,
@@ -19,35 +20,40 @@ export default function TodoItem({ todo, count }) {
       className="md:-[#1f1f1f] bg-opacity-40 py-3 px-3 relative flex items-center justify-between gap-3 border-b border-[#383838]"
     >
       <div className="flex  items-start gap-3 w-full">
-        <input
-          type="checkbox"
-          className="mt-1"
-          checked={todo.completed === 1}
-          onChange={() =>
-            handleUpdateTodo({
-              ...todo,
-              completed: todo.completed === 0 ? 1 : 0,
-            })
-          }
-        />
-
         <div className="flex gap-2 max-w-[90%] relative">
           <p className="flex items-center gap-2">{todo.title}</p>
         </div>
 
-        {count <=5 && <p className={`font-bold text-gray-100 ${isFocused ? 'opacity-80' : 'opacity-5'} absolute left-[-5px]`}>{count}</p>}
+        {count <= 10 && (
+          <p
+            className={`font-bold text-gray-100 ${isFocused ? 'opacity-80' : 'opacity-5'} absolute left-[-15px]`}
+          >
+            {count}
+          </p>
+        )}
       </div>
 
       {isFocused && (
         <div className="flex items-center gap-5">
           <button
-            className="absolute right-20"
+            className="absolute right-[105px] text-green-500"
+            onClick={() =>
+              handleUpdateTodo({
+                ...todo,
+                completed: todo.completed === 0 ? 1 : 0,
+              })
+            }
+          >
+            <GoCheckCircle size={16} />
+          </button>
+          <button
+            className="absolute right-[70px]"
             onClick={() => handleUpdateTodo({ ...todo })}
           >
             <GoFoldUp size={20} />
           </button>
           <button
-            className="absolute right-12"
+            className="absolute right-[40px]"
             onClick={() =>
               handleUpdateTodo({ ...todo, backlog: todo.backlog === 0 ? 1 : 0 })
             }
@@ -55,7 +61,7 @@ export default function TodoItem({ todo, count }) {
             <GoArrowSwitch size={16} />
           </button>
           <button
-            className="absolute right-3"
+            className="absolute right-[6px]"
             onClick={() => handleDeleteTodo(todo.id)}
           >
             <GoTrash size={13} />
