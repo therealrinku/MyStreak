b3871747dd21fa4a70dd734d96fafc50c606edfc
therealrinku <rinkunited2032@gmail.com>
2026-02-14 22:43:11 +0545

    Add custom toggle component

--- src/renderer/components/settings.tsx
+++ src/renderer/components/settings.tsx
@@ -1,31 +1,24 @@
 import { GoCheck } from 'react-icons/go';
+import ToggleSwitch from './toggle-switch';
 
 export default function Settings() {
   return (
     <div>
       <div className="md:-[#1f1f1f] bg-opacity-40 py-3 px-3 relative flex items-center justify-between gap-3 border-b border-[#383838]">
         <p>Show completed todos tab</p>
-        <button>
-          <GoCheck />
-        </button>
+        <ToggleSwitch />
       </div>
       <div className="md:-[#1f1f1f] bg-opacity-40 py-3 px-3 relative flex items-center justify-between gap-3 border-b border-[#383838]">
         <p>Show todos serial number</p>
-        <button>
-          <GoCheck />
-        </button>
+        <ToggleSwitch />
       </div>
       <div className="md:-[#1f1f1f] bg-opacity-40 py-3 px-3 relative flex items-center justify-between gap-3 border-b border-[#383838]">
         <p>Show move to top button</p>
-        <button>
-          <GoCheck />
-        </button>
+        <ToggleSwitch />
       </div>
       <div className="md:-[#1f1f1f] bg-opacity-40 py-3 px-3 relative flex items-center justify-between gap-3 border-b border-[#383838]">
         <p>Show complete todo button</p>
-        <button>
-          <GoCheck />
-        </button>
+        <ToggleSwitch />
       </div>
     </div>
   );
---
+++ src/renderer/components/toggle-switch.tsx
@@ -0,0 +1,8 @@
+export default function ToggleSwitch() {
+  return (
+    <label className="switch">
+      <input type="checkbox" />
+      <span className="slider"></span>
+    </label>
+  );
+}
